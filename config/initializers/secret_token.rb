6e0adbb5690779d35a7a6f1d1ba72ffb8f6d03f7
Ionut Pavlencu <ionut.pavlencu@gmail.com>
2014-02-18 19:48:31 +0200

    Incarcare initiala a datelor

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+InvatRuby::Application.config.secret_key_base = 'dbfe6ec6aa09cdefa0c758a51961827d484532a49476af7c226977efdad839c6f4fb18fdaaa69a9d232c7162d487d9d68b985fef529e0d30d92438f6f06f2b8b'
