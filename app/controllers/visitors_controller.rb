4461b22125e13adcf322fbe90f71c4249fca655b
Ionut Pavlencu <ionut.pavlencu@gmail.com>
2014-02-23 14:28:17 +0200

    front end framework facut cu Zurb Foundation

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,8 +3,8 @@ class VisitorsController < ApplicationController
 
 		@owner = Owner.new
 		#render 'visitors/new'
-		flash.now[:notice] = "Bine ai venit!!!"
-		flash.now[:alert] = "Pana la ziua mea mai sunt #{@owner.ziler} zile!!!"
+		#flash.now[:notice] = "Bine ai venit!!!"
+		#flash.now[:alert] = "Pana la ziua mea mai sunt #{@owner.ziler} zile!!!"
 	end
 	
 	
